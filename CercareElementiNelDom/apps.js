aab3320165d7b8f3763f383f0b257a5ebcd5b4a3
circumspectjunior <giftojo200047@yahoo.com>
2024-09-30 18:13:29 +0200

    cercareElementi_added

---
+++ CercareElementiNelDom/apps.js
@@ -0,0 +1,7 @@
+/*Cerca tutti gli elementi con classe "paragrafo".
+Cerca l' elemento con id "elemento-3"
+Cerca tutti gli elementi h2 della pagina */
+
+const p = document.getElementsByClassName("paragrafo");
+const elemento3 = document.getElementById("elemento-3");
+const h2 = document.getElementsByTagName("h2");
