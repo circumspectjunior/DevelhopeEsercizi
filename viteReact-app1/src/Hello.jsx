fa9763a44fdd63c272a718eae0e76397e1a7f485
circumspectjunior <giftojo200047@yahoo.com>
2024-10-08 11:57:02 +0200

    message-component-added

--- viteReact-app1/src/Hello.jsx
+++ viteReact-app1/src/Hello.jsx
@@ -1,10 +1,16 @@
+import Message from "./Message";
 
 
 export default function Hello(){
 
+    //Message component can be used more than once put can't have more than one parent.
+
     return(
         <div>
             <h2>Hello.World</h2>
+            <Message />
+            <Message />
+
         </div>
     )
 }
---
+++ viteReact-app1/src/Message.jsx
@@ -0,0 +1,9 @@
+
+export default function Message() {
+
+    return(
+        <p>
+            What a beautiful day!
+        </p>
+    )
+}
